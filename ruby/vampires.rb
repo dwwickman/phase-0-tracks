fb3da57379b8e0e24786d265d35c0032fc215ec5
Daniel Wickman <dwwickman@me.com>
2017-03-07 17:07:30 -0500

    Added boolean for garlic bread and Insurance.  Also printed variables for testing

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -3,7 +3,7 @@ name = gets.chomp
 puts "How old are you?"
 age = gets.chomp.to_i
 puts "What year were you born?"
-year_born = get.chomp.to_i
+year_born = gets.chomp.to_i
 actual_age = 2017 - year_born
 if age == actual_age
 	correct_age = true
@@ -12,6 +12,24 @@ else
 end
 puts "Our company cafeteria serves garlic bread.  Should we order some for you (Yes/No)?"
 garlic_bread_response = gets.chomp
+if garlic_bread_response == "Yes"
+	garlic_bread_wanted = true
+else
+	garlic_bread_wanted = false
+end
 puts "Would you like to enroll in the company's health insurance? (Yes/No)"
-health_insurance_wanted = gets.chomp
+health_insurance_resonse = gets.chomp
+if health_insurance_resonse == "Yes"
+	health_insurance_wanted = true
+else
+	health_insurance_wanted = false
+end
+p name
+p age
+p year_born
+p correct_age
+p garlic_bread_response
+p garlic_bread_wanted
+p health_insurance_resonse
+p health_insurance_wanted
 
