df00a4727e1cbab1085342310b39c137cb78e84d
Daniel Wickman <dwwickman@me.com>
2017-03-10 11:50:14 -0500

    Added encypt method to program and tested it

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -3,3 +3,19 @@
 #make a loop change index.  use next method to switch character.  if statment for blank characters and if statement for z so it doesn't go to aa
 #decrypt
 #compare updated password to "abcdefghijklmnoprstuvwxyz".  If letters match move index in alpabet back by 1.  If statment for a. If statment for " ".
+def encrypt(password)
+	index = 0
+	while index < password.length
+		if password[index] == " "
+			password[index] = " "
+			elsif password[index] == "z"
+				password[index] = "a"
+			else
+				password[index] = password[index].next
+		end
+		index += 1
+	end
+	password
+end
+
+puts encrypt(" zd")
