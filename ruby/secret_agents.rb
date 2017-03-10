d17d039982460f3fdf8bf9072e8b1accf137be3f
Daniel Wickman <dwwickman@me.com>
2017-03-10 11:43:09 -0500

    Added pseudocode for encrypt and decrypt

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,5 @@
+#psuedocode
+#encrypt
+#make a loop change index.  use next method to switch character.  if statment for blank characters and if statement for z so it doesn't go to aa
+#decrypt
+#compare updated password to "abcdefghijklmnoprstuvwxyz".  If letters match move index in alpabet back by 1.  If statment for a. If statment for " ".
