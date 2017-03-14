7e2e4a8707095eff36cd4f95fdaff061f6ac2cd5
Daniel Wickman <dwwickman@me.com>
2017-03-14 16:23:13 -0500

    Finish release 1 by add array/hash.  Use .each and .map

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -1,6 +1,41 @@
-def fav_movie
 	puts "I LOVE MOVIES"
+def fav_movie
 	yield("Scream", "Rosemary's Baby")
 end
 
 fav_movie { |movie1, movie2| puts "2 of them are:  #{movie1} and #{movie2}"}
+
+
+
+movie_genres = ["Horror", "Thriller", "Comedy", "Drama", "Romance"]
+
+
+puts "Original Data:"
+p movie_genres
+
+movie_genres.each do |genre|
+	puts genre
+end
+
+puts "After . each call:"
+p movie_genres
+
+movie_genres.map! do |genre|
+	puts genre
+	genre.upcase
+
+end
+
+puts "After map call"
+p movie_genres
+
+
+
+##########
+
+genre_ranking = {1 => 'Horror', 2 => 'Comedy', 3 => 'Drama'}
+
+genre_ranking.each do |ranking, genre|
+puts "#{genre} is ranked # #{ranking} on my list"
+
+end
