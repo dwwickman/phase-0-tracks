1899e74e0ed602a25c106ca65571c9ca8e280709
Daniel Wickman <dwwickman@me.com>
2017-03-14 16:47:29 -0500

    Finish release 2.  Use new methods for arrays and hashes

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -35,7 +35,35 @@ p movie_genres
 
 genre_ranking = {1 => 'Horror', 2 => 'Comedy', 3 => 'Drama'}
 
+puts "Before:"
+
+p genre_ranking
+
 genre_ranking.each do |ranking, genre|
 puts "#{genre} is ranked # #{ranking} on my list"
 
 end
+
+puts "After:"
+
+p genre_ranking
+
+############
+
+numbers_array = [1, 2, 3, 4, 5, 6, 7, 8]
+letters_hash = { "a" => 1, "b" => 2, "c" =>3 }
+
+puts "before:"
+
+p numbers_array
+p letters_hash
+
+numbers_array.reject! { |a| a < 5 }
+letters_hash.delete("a")
+
+
+puts "After method"
+
+p numbers_array
+p letters_hash
+
