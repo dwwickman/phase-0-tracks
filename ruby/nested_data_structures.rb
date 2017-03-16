65146a14617c6c633e4a5063bbd3bff89c6deead
Daniel Wickman <dwwickman@me.com>
2017-03-16 09:58:29 -0500

    Add nested structures for TV Shows

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,14 @@
+TV_Shows = {
+	"NBC" => ["Blacklist", "Blindspot", "Chicago P.D.", "This is Us"],
+	"USA" => ["Modern Famiy", "Mr Robot", "Suits", "Shooter"],
+	"AMC" => ["Walking Dead", "Into the Badlands", "Fear the Walking Dead"],
+	"CW" => ["Arrow", "Flash", "Supergirl", "Vampire Diaries"]
+}
+
+p TV_Shows
+p TV_Shows["CW"]
+p TV_Shows["USA"]
+p TV_Shows["CW"][0]
+p TV_Shows["AMC"][0]
+p TV_Shows["USA"][2]
+p TV_Shows["NBC"][2]
