a5a8bd6963fdf1bb12426e0f4465568e1ae29a8c
Daniel Wickman <dwwickman@me.com>
2017-03-22 22:57:31 -0500

    Add instance variable attributes

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,7 +1,11 @@
 class Santa
 
-	def initialize
+	def initialize(gender, ethnicity)
 		puts "Initializing Santa instance..."
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0		
 	end
 
 def speak
@@ -14,7 +18,8 @@ end
 
 end
 
-og_santa = Santa.new
+og_santa = Santa.new("agender", "mystical creature")
 
 og_santa.speak
 og_santa.eat_milk_and_cookies("Chocolate Chip")
+
