f87014d1184db71cd78c0ee6ec7c820ec0cce0e3
Daniel Wickman <dwwickman@me.com>
2017-03-23 12:43:56 -0500

    Comment out old code.  Add Shout module, Football and Baskeball class.  Made isntance of each.  Called each instance

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,43 @@
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+# def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+
+# end
+
+# puts Shout.yell_angrily("SO MAD")
+# puts Shout.yell_happily("SO HAPPY")
+
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+
+	def yell_angrily(words)
+    words.upcase + "!!!" + " :("
   end
 
-def self.yell_happily(words)
-    words + "!!!" + " :)"
+  def yell_happily(words)
+    words.upcase + "!!!" + " :("
   end
-
 end
 
-puts Shout.yell_angrily("SO MAD")
-puts Shout.yell_happily("SO HAPPY")
+class Football
+	include Shout
+end
 
+class Basketball
+	include Shout
+end
+
+football_player = Football.new
+
+puts football_player.yell_happily("I got a touchdown")
+puts football_player.yell_angrily("I Threw an Interception")
+
+basketball_player = Basketball.new
+
+puts basketball_player.yell_happily("I made a basketball")
+puts basketball_player.yell_angrily("I turned the ball over")
