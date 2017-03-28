06cd2cb5d8caf41f5594e0cc65e72791fe6b6b40
Daniel Wickman <dwwickman@me.com>
2017-03-28 08:14:37 -0500

    Add loop for reverse string

--- js/explore.js
+++ js/explore.js
@@ -4,3 +4,13 @@
 //			Loop thrugh string backwards.
 //			Store each character in the blank array
 //output:  reverse_string
+
+var str = "test";
+
+var reverse_string = '';
+
+for (i = str.length - 1; i >= 0; i--) {
+	reverse_string += str[i]
+}
+
+console.log(reverse_string);
