53eb628114128317c69c244911fd4d9d1a9b94af
Daniel Wickman <dwwickman@me.com>
2017-03-29 11:55:18 -0500

    Add pseudocode

---
+++ js/algos.js
@@ -0,0 +1,10 @@
+//pseudocode
+// input: array
+//	steps:  create a longest_word variable to store longest variable
+//  		loop through array
+//			if current word length is longer than previous length replace
+// output:  longest_word
+
+var array = ["long phrase","longest phrase","longer phrase"];
+
+console.log(array)
