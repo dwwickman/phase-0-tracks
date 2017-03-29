304f8ac833b48fbae4e90acdd7cdf26d86b93b5d
Daniel Wickman <dwwickman@me.com>
2017-03-29 12:02:07 -0500

    Add function that return longest word/phrase of an array

--- js/algos.js
+++ js/algos.js
@@ -5,6 +5,19 @@
 //			if current word length is longer than previous length replace
 // output:  longest_word
 
-var array = ["long phrase","longest phrase","longer phrase"];
 
-console.log(array)
+
+function long(array) {
+var longest_word = "";
+
+for (i = 0; i < array.length; i++) {
+	if (array[i].length > longest_word.length) {
+		longest_word = array[i];
+	}
+}
+return longest_word
+}
+
+var array = ["long phraseasdfadsfasdfa","longest phrase","longer phrasexxxxxxx"];
+
+console.log(long(array));
